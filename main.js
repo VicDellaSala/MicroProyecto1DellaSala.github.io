c0db5ded0e3b65f1afc162a45f4d838c45e0a5cb
VicDellaSala <113453944+VicDellaSala@users.noreply.github.com>
2024-02-11 18:57:17 +0100

    Todos los cartones listo

--- main.js
+++ main.js
@@ -64,6 +64,66 @@ function TamañoCarton3x3_1(){
         }
 }  
 
+function TamañoCarton3x3_2(){
+    let div = document.getElementById('carton3x3_2');
+    div.innerHTML = '';
+        let matrix_3x3 = [];
+        for (let i = 0; i < 3; i++) {
+            let row = [];
+            for (let j = 0; j < 3; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_3x3.push(row);
+        }
+        console.log(matrix_3x3);
+        for (let i = 0; i < matrix_3x3.length; i++) {
+            for (let j = 0; j < matrix_3x3[i].length; j++) {
+                div.innerHTML += `<div>${matrix_3x3[i][j]}</div>`;
+            }
+        }
+}  
+
+function TamañoCarton3x3_3(){
+    let div = document.getElementById('carton3x3_3');
+    div.innerHTML = '';
+        let matrix_3x3 = [];
+        for (let i = 0; i < 3; i++) {
+            let row = [];
+            for (let j = 0; j < 3; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_3x3.push(row);
+        }
+        console.log(matrix_3x3);
+        for (let i = 0; i < matrix_3x3.length; i++) {
+            for (let j = 0; j < matrix_3x3[i].length; j++) {
+                div.innerHTML += `<div>${matrix_3x3[i][j]}</div>`;
+            }
+        }
+}  
+
+function TamañoCarton3x3_4(){
+    let div = document.getElementById('carton3x3_4');
+    div.innerHTML = '';
+        let matrix_3x3 = [];
+        for (let i = 0; i < 3; i++) {
+            let row = [];
+            for (let j = 0; j < 3; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_3x3.push(row);
+        }
+        console.log(matrix_3x3);
+        for (let i = 0; i < matrix_3x3.length; i++) {
+            for (let j = 0; j < matrix_3x3[i].length; j++) {
+                div.innerHTML += `<div>${matrix_3x3[i][j]}</div>`;
+            }
+        }
+}  
+
 let matrix_4x4 = [
     [],
     [],
@@ -90,7 +150,67 @@ function TamañoCarton4x4_1(){
             }
         }
 }  
- 
+
+function TamañoCarton4x4_2(){
+    let div = document.getElementById('carton4x4_2');
+    div.innerHTML = '';
+        let matrix_4x4 = [];
+        for (let i = 0; i < 4; i++) {
+            let row = [];
+            for (let j = 0; j < 4; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_4x4.push(row);
+        }
+        console.log(matrix_4x4);
+        for (let i = 0; i < matrix_4x4.length; i++) {
+            for (let j = 0; j < matrix_4x4[i].length; j++) {
+                div.innerHTML += `<div>${matrix_4x4[i][j]}</div>`;
+            }
+        }
+} 
+
+function TamañoCarton4x4_3(){
+    let div = document.getElementById('carton4x4_3');
+    div.innerHTML = '';
+        let matrix_4x4 = [];
+        for (let i = 0; i < 4; i++) {
+            let row = [];
+            for (let j = 0; j < 4; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_4x4.push(row);
+        }
+        console.log(matrix_4x4);
+        for (let i = 0; i < matrix_4x4.length; i++) {
+            for (let j = 0; j < matrix_4x4[i].length; j++) {
+                div.innerHTML += `<div>${matrix_4x4[i][j]}</div>`;
+            }
+        }
+} 
+
+function TamañoCarton4x4_4(){
+    let div = document.getElementById('carton4x4_4');
+    div.innerHTML = '';
+        let matrix_4x4 = [];
+        for (let i = 0; i < 4; i++) {
+            let row = [];
+            for (let j = 0; j < 4; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_4x4.push(row);
+        }
+        console.log(matrix_4x4);
+        for (let i = 0; i < matrix_4x4.length; i++) {
+            for (let j = 0; j < matrix_4x4[i].length; j++) {
+                div.innerHTML += `<div>${matrix_4x4[i][j]}</div>`;
+            }
+        }
+} 
+
 let matrix_5x5 = [
     [],
     [],
@@ -119,7 +239,65 @@ function TamañoCarton5x5_1(){
         }
 }  
 
+function TamañoCarton5x5_2(){
+    let div = document.getElementById('carton5x5_2');
+    div.innerHTML = '';
+        let matrix_5x5 = [];
+        for (let i = 0; i < 5; i++) {
+            let row = [];
+            for (let j = 0; j < 5; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_5x5.push(row);
+        }
+        console.log(matrix_5x5);
+        for (let i = 0; i < matrix_5x5.length; i++) {
+            for (let j = 0; j < matrix_5x5[i].length; j++) {
+                div.innerHTML += `<div>${matrix_5x5[i][j]}</div>`;
+            }
+        }
+} 
 
+function TamañoCarton5x5_3(){
+    let div = document.getElementById('carton5x5_3');
+    div.innerHTML = '';
+        let matrix_5x5 = [];
+        for (let i = 0; i < 5; i++) {
+            let row = [];
+            for (let j = 0; j < 5; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_5x5.push(row);
+        }
+        console.log(matrix_5x5);
+        for (let i = 0; i < matrix_5x5.length; i++) {
+            for (let j = 0; j < matrix_5x5[i].length; j++) {
+                div.innerHTML += `<div>${matrix_5x5[i][j]}</div>`;
+            }
+        }
+} 
+
+function TamañoCarton5x5_4(){
+    let div = document.getElementById('carton5x5_4');
+    div.innerHTML = '';
+        let matrix_5x5 = [];
+        for (let i = 0; i < 5; i++) {
+            let row = [];
+            for (let j = 0; j < 5; j++) {
+                let randomNumber = Math.floor(Math.random() * 50);
+                row.push(randomNumber);
+            }
+            matrix_5x5.push(row);
+        }
+        console.log(matrix_5x5);
+        for (let i = 0; i < matrix_5x5.length; i++) {
+            for (let j = 0; j < matrix_5x5[i].length; j++) {
+                div.innerHTML += `<div>${matrix_5x5[i][j]}</div>`;
+            }
+        }
+} 
 
 // const maximo = 5 Nunca cambbia
 // let contador = 0 Cambia
